c96327cc4938bb678b3366aa96228e0523bc4bd8
Maximilian Huber <gh@maxhbr.de>
2019-09-09 14:37:41 +0200

    WIP
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- code/rail/src/main.cpp
+++ code/rail/src/main.cpp
@@ -6,12 +6,9 @@
 const int button_move_forward = 16;
 const int button_move_backward = 4;
 const int button_single_step = 17;
-// const int button_modehalfer = 5;
 
 Rail rail(32, 33, 25, 26, 27, 14, 12);
-IrSony ir_sony(2);
-
-int mode = 1;
+// IrSony ir_sony(0);
 
 void setup() {
     Serial.begin(9600);
@@ -19,23 +16,24 @@ void setup() {
     pinMode(button_move_forward, INPUT);
     pinMode(button_move_backward, INPUT);
     pinMode(button_single_step, INPUT);
-    // pinMode(button_modehalfer, INPUT);
 }
 
 void loop() {
     if (digitalRead(button_single_step) == HIGH)
     {
-        ir_sony.shoot();
+        // ir_sony.shoot();
         rail.move(15, 0);
         delay(700);
     }
     else if (digitalRead(button_move_forward) == HIGH)
     {
+        Serial.write("forward");
         // rail.move(32);
         rail.step(32);
     }
     else if (digitalRead(button_move_backward) == HIGH)
     {
+        Serial.write("backward");
         // rail.move(-32);
         rail.step(-32);
     }
