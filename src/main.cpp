c1f4dce1457b11faf0f71c30ee276cc3150c6557
Stefan Hackenberg <mail@stefan-hackenberg.de>
2019-09-21 18:20:11 +0200

    Experiment with FreeRTOS tasks and async web server handling
    E.g. 2 full loops divided into 80 requests result in a smooth movement.
    for _ in range(80): requests.post('http://192.168.1.104/move', data=b'\x00\x00\x00\xa0\x00\x00\x00\x03')

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
 
 #include "PhotoRail.h"
 
@@ -7,11 +8,62 @@ const int button_move_backward = 4;
 const int button_start = 17;
 const int button_alt = 5;
 const int status_led = 15;
+const char *ssid = "***";
+const char *password = "***";
 
 PhotoRail rail(32, 33, 25, 26, 27, 14, 12, 2);
 
+AsyncWebServer server(80);
+QueueHandle_t queue;
+
+void dotask(void *parameter)
+{
+    while (1)
+    {
+        int distance;
+        xQueueReceive(queue, &distance, portMAX_DELAY);
+        rail.move(distance, 3);
+    }
+}
+
 void setup() {
     Serial.begin(9600);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED);
+    Serial.println(WiFi.localIP());
+
+    queue = xQueueCreate(2, sizeof(int));
+
+    server.on(
+        "/move",
+        HTTP_POST,
+        [](AsyncWebServerRequest *request){},
+        NULL,
+        [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
+            if (len != 2 * sizeof(uint32_t))
+            {
+                request->send(400);
+                return;
+            }
+            int distance = (int) ntohl(*(uint32_t *)data);
+            unsigned int max_speed = ntohl(*(uint32_t *)(data + 4));
+            // Serial.println("post: distance = " + String(distance));
+            // rail.move(distance, max_speed);
+            xQueueSend(queue, &distance, portMAX_DELAY);
+            request->send(200);
+        });
+    server.begin();
+
+    xTaskCreate(
+        dotask,                  /* pvTaskCode */
+        "Workload1",            /* pcName */
+        10000,                   /* usStackDepth */
+        NULL,                   /* pvParameters */
+        3,                      /* uxPriority */
+        NULL);                /* pxCreatedTask */
+
 
     pinMode(button_move_forward, INPUT);
     pinMode(button_move_backward, INPUT);
