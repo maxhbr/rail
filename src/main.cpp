8677464ea2f9543fa2baa41cebb2d6d4ff34da93
Maximilian Huber <gh@maxhbr.de>
2020-04-19 21:05:34 +0200

    update pin layout
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@
 
 const int button_move_forward = 16;
 const int button_move_backward = 4;
-const int button_start = 17;
-const int button_alt = 5;
-const int status_led = 15;
+const int button_start = 5;
+const int button_alt = 17;
+const int button_top_1 = 34;
+const int button_top_2 = 35;
+const int status_led = 2;
 
-PhotoRail rail(32, 33, 25, 26, 27, 14, 12, 2);
+PhotoRail rail(32, // pin_direction
+               33, // pin_step
+               25, // pin_sleep
+               26, // pin_reset
+               27, // pin_mode2
+               14, // pin_mode1
+               12, // pin_mode0
+               15  // pin_ir
+               );
 
 void setup() {
     Serial.begin(9600);
@@ -16,6 +26,10 @@ void setup() {
     pinMode(button_move_forward, INPUT);
     pinMode(button_move_backward, INPUT);
     pinMode(button_start, INPUT);
+    pinMode(button_alt, INPUT);
+    pinMode(button_top_1, INPUT);
+    pinMode(button_top_1, INPUT);
+
     pinMode(status_led, OUTPUT);
 
     digitalWrite(status_led, LOW);
